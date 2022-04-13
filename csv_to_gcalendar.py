13288c7a638087c928c21994ec6264465d5f1b54
Jorge Eduardo de Sousa Aguiar <sousa.aguiar@gmail.com>
2022-04-13 14:56:50 -0300

    Atualiza modelo e reformula código para checar a existência dos arquivos necessários

--- csv_to_gcalendar.py
+++ csv_to_gcalendar.py
@@ -1,6 +1,16 @@
 import pandas as pd
+import os
 
-df = pd.read_excel("modelo_calendario.xlsx")
+try:
+    df = pd.read_excel("modelo_calendario.xlsx")
+except FileNotFoundError:
+    print("Arquivo modelo não encontrado.")
+    exit()
+if os.path.exists("importar.csv"):
+    os.remove("importar.csv")
 
-print(df)
 df.to_csv("importar.csv", sep=",", encoding="latin-1", index=False)
+if os.path.exists("importar.csv"):
+    print("Arquivo importar.csv gerado com sucesso!")
+else:
+    print("Não foi possível gerar o arquivo 'importar.csv'.")
